0e7ff62537ac5f8f3c297ca2117dce183de09caf
austinbhan <austinbhan@outlook.com>
2022-06-10 14:34:49 -0700

    Sign Section.js

--- src/SignSection.js
+++ src/SignSection.js
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
 import Sign from './Sign';
 import CustomButton from './CustomButton';
+import React, { useState } from 'react';
 
 export default function SignSection() {
-    const [isOpen, setIsOPen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    return(
-        <section className = 'sign'>
-            // Continue with Sign
-        </section>
+  return (
+    <section className = 'sign'>
+      <Sign isOpen={isOpen} />
+      <div>
+        <CustomButton onClick={() => setIsOpen(false)}>Close Zoo</CustomButton>
+        <CustomButton onClick={() => setIsOpen(true)}>Open Zoo</CustomButton>
+      </div>
 
-
-
-    );
+    </section>
+  );
 }
