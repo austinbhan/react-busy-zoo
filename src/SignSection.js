4d6e2e60faee133b247c097f562639ff924d4df7
austinbhan <austinbhan@outlook.com>
2022-06-10 14:31:54 -0700

    Created Sign.js

---
+++ src/SignSection.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Sign from './Sign';
+import CustomButton from './CustomButton';
+
+export default function SignSection() {
+    const [isOpen, setIsOPen] = useState(false);
+
+    return(
+        <section className = 'sign'>
+            // Continue with Sign
+        </section>
+
+
+
+    );
+}
