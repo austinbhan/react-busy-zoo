18ca6c38a56f8907b29b7c522abb799800f5a6e8
austinbhan <austinbhan@outlook.com>
2022-07-01 16:32:09 -0700

    Data.js

--- src/Admin/Data.js
+++ src/Admin/Data.js
@@ -1,1 +1,212 @@
 // Mockaroo and Victory Data Go Here
+const namesTable = [{
+  'id': 1,
+  'first_name': 'Daron',
+  'last_name': 'Sawnwy'
+}, {
+  'id': 2,
+  'first_name': 'Barnabas',
+  'last_name': 'Paul'
+}, {
+  'id': 3,
+  'first_name': 'Rafi',
+  'last_name': 'Couch'
+}, {
+  'id': 4,
+  'first_name': 'Martguerita',
+  'last_name': 'Saintsbury'
+}, {
+  'id': 5,
+  'first_name': 'Alexi',
+  'last_name': 'MacNeilly'
+}, {
+  'id': 6,
+  'first_name': 'Carrissa',
+  'last_name': 'Tuma'
+}, {
+  'id': 7,
+  'first_name': 'Fonzie',
+  'last_name': 'Jakobssen'
+}, {
+  'id': 8,
+  'first_name': 'Lorry',
+  'last_name': 'Syalvester'
+}, {
+  'id': 9,
+  'first_name': 'Ethelred',
+  'last_name': 'Scranedge'
+}, {
+  'id': 10,
+  'first_name': 'Odetta',
+  'last_name': 'Baldetti'
+}, {
+  'id': 11,
+  'first_name': 'Stesha',
+  'last_name': 'Spurgeon'
+}, {
+  'id': 12,
+  'first_name': 'Kingsley',
+  'last_name': 'Bulleyn'
+}, {
+  'id': 13,
+  'first_name': 'Coreen',
+  'last_name': 'Kyllford'
+}, {
+  'id': 14,
+  'first_name': 'Swen',
+  'last_name': 'Boothe'
+}, {
+  'id': 15,
+  'first_name': 'Ezequiel',
+  'last_name': 'Canlin'
+}, {
+  'id': 16,
+  'first_name': 'Odelle',
+  'last_name': 'Luck'
+}, {
+  'id': 17,
+  'first_name': 'Myrta',
+  'last_name': "O'Leahy"
+}, {
+  'id': 18,
+  'first_name': 'Iseabal',
+  'last_name': "O' Culligan"
+}, {
+  'id': 19,
+  'first_name': 'Courtnay',
+  'last_name': 'Kimber'
+}, {
+  'id': 20,
+  'first_name': 'Ash',
+  'last_name': 'Nashe'
+}
+];
+
+const fakeCreditCards = [{
+  'id': 1,
+  'first-name': 'Charil',
+  'credit-card': '30035249929597',
+  'balance': '$5.74',
+  'ip_address': '187.225.229.6'
+}, {
+  'id': 2,
+  'first-name': 'Torre',
+  'credit-card': '3583286035280050',
+  'balance': '$3.39',
+  'ip_address': '209.120.166.156'
+}, {
+  'id': 3,
+  'first-name': 'Isabelita',
+  'credit-card': '3567572695153772',
+  'balance': '$7.58',
+  'ip_address': '251.214.154.124'
+}, {
+  'id': 4,
+  'first-name': 'Florinda',
+  'credit-card': '6763131063361825',
+  'balance': '$4.83',
+  'ip_address': '176.119.192.184'
+}, {
+  'id': 5,
+  'first-name': 'Lucia',
+  'credit-card': '676192059159160184',
+  'balance': '$8.15',
+  'ip_address': '22.151.84.69'
+}, {
+  'id': 6,
+  'first-name': 'Reggy',
+  'credit-card': '3545650790434839',
+  'balance': '$2.60',
+  'ip_address': '215.190.244.194'
+}, {
+  'id': 7,
+  'first-name': 'Midge',
+  'credit-card': '3549905268410505',
+  'balance': '$5.09',
+  'ip_address': '55.112.78.50'
+}, {
+  'id': 8,
+  'first-name': 'Jermaine',
+  'credit-card': '3559944169499843',
+  'balance': '$5.82',
+  'ip_address': '153.167.93.86'
+}, {
+  'id': 9,
+  'first-name': 'Judon',
+  'credit-card': '4844815389170740',
+  'balance': '$3.71',
+  'ip_address': '107.147.154.120'
+}, {
+  'id': 10,
+  'first-name': 'Amabel',
+  'credit-card': '3550737904288524',
+  'balance': '$2.71',
+  'ip_address': '185.186.49.244'
+}, {
+  'id': 11,
+  'first-name': 'Edgard',
+  'credit-card': '6759147050865840817',
+  'balance': '$0.73',
+  'ip_address': '208.32.23.33'
+}, {
+  'id': 12,
+  'first-name': 'Willow',
+  'credit-card': '3586558129895580',
+  'balance': '$7.71',
+  'ip_address': '99.36.103.80'
+}, {
+  'id': 13,
+  'first-name': 'Babita',
+  'credit-card': '3557141754201682',
+  'balance': '$3.87',
+  'ip_address': '13.168.121.17'
+}, {
+  'id': 14,
+  'first-name': 'Yoshi',
+  'credit-card': '30003224831170',
+  'balance': '$4.79',
+  'ip_address': '140.247.239.234'
+}, {
+  'id': 15,
+  'first-name': 'Brit',
+  'credit-card': '3560545801736778',
+  'balance': '$5.61',
+  'ip_address': '77.210.104.64'
+}, {
+  'id': 16,
+  'first-name': 'Tracey',
+  'credit-card': '5503422780930373',
+  'balance': '$8.68',
+  'ip_address': '40.114.97.242'
+}, {
+  'id': 17,
+  'first-name': 'Willdon',
+  'credit-card': '3551133306996295',
+  'balance': '$4.49',
+  'ip_address': '126.186.193.78'
+}, {
+  'id': 18,
+  'first-name': 'Wanda',
+  'credit-card': '6759419520492226',
+  'balance': '$0.49',
+  'ip_address': '118.87.86.88'
+}, {
+  'id': 19,
+  'first-name': 'Lynde',
+  'credit-card': '201717221239268',
+  'balance': '$4.84',
+  'ip_address': '249.17.12.74'
+}, {
+  'id': 20,
+  'first-name': 'Saw',
+  'credit-card': '3531524217177121',
+  'balance': '$9.64',
+  'ip_address': '204.133.160.163'
+}];
+
+const chartData = [
+  { quarter: 1, earnings: 13000 },
+  { quarter: 2, earnings: 16500 },
+  { quarter: 3, earnings: 14250 },
+  { quarter: 4, earnings: 19000 }
+];
