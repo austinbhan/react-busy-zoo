9132b003e9226e9fe7030fa459fbd784acad289d
austinbhan <austinbhan@outlook.com>
2022-07-01 17:23:15 -0700

    Names rendering

--- src/Admin/MyTable.js
+++ src/Admin/MyTable.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { namesTable } from './Data';
 
 export default function MyTable() {
+
+  
+  console.log(namesTable);
+  
   return (
-    <div>
-            MyTable has been successfully imported
-    </div>
+    <>
+      <div>
+        {
+          namesTable.map((person, i) => <p key={person.first_name + i}>
+            {person.id}
+            {person.first_name}
+            {person.last_name} </p>)
+        }
+        Table One
+      </div>
+      <div>
+        Table Two
+      </div>
+    </>
   );
 }
